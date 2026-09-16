9dd971bae4e3c1d65aec89992a11c040478a4204
Sebastian Bila <sebastian@xicay.com>
2026-09-16 13:33:00 +0300

    initial commit

---
+++ Game.cs
@@ -0,0 +1,11 @@
+namespace KN_22_Git_Test;
+
+public class Game
+{
+    public void Start()
+    {
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine("Game is starting");
+        Console.ResetColor();
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System.Resources;
+
+namespace KN_22_Git_Test;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var game = new Game();
+        game.Start();
+        Console.WriteLine("KN_22 Testing Git!");
+    }
+}
